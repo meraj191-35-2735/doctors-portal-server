d799dfc8bb74b5bb3ca7059d155a0333e7f0aad5
meraj191-35-2735 <www.shamsad35-2735@diu.edu.bd>
2022-05-21 13:57:09 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -25,6 +26,26 @@ async function run() {
     const bookingCollection = client
       .db("doctors_portal")
       .collection("bookings");
+
+    const userCollection = client.db("doctors_portal").collection("users");
+
+    //add or update user info
+    app.put("/user/:email", async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign(
+        { email: email },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.send({ result, token });
+    });
 
     app.get("/service", async (req, res) => {
       const query = {};
